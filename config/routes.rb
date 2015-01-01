07c954d5654e89a1821d137024c60c3659b817f8
Mahhek <dell@dell-Inspiron-N5010.(none)>
2015-01-01 18:37:06 +0500

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,13 @@ Rails.application.routes.draw do
     resources :companies 
   end
   
-  constraints(Subdomain) do
-    get '/' => 'home#index'
-  end
-
   resources :vehicle_types
   resources :reservation_settings
+  #  do
+  #    collection do
+  #      get "reservation_setting"
+  #    end
+  #  end
   get "/companies/reservation_settings" => "reservation_settings#reservation_setting"
-
 
 end
