922cb59849e7366f6dea24fa1cb12ae886186566
Hamid <hamid@hamid>
2015-01-05 15:56:19 +0500

    delete zone and zip code

--- app/controllers/company_zones_controller.rb
+++ app/controllers/company_zones_controller.rb
@@ -1,6 +1,6 @@
 class CompanyZonesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_company_zone , only: :show
+  before_action :set_company_zone , only: [:show,:destroy]
   def show
     
   end
@@ -8,6 +8,10 @@ class CompanyZonesController < ApplicationController
   def create
     @company_zone = CompanyZone.new(company_zone_params)
     @company_zone.save
+  end
+  def destroy
+    @company_zone.destroy
+    @company_zone = current_user.company.company_zones.first
   end
   private
   def company_zone_params
--- app/controllers/zip_codes_controller.rb
+++ app/controllers/zip_codes_controller.rb
@@ -1,11 +1,20 @@
 class ZipCodesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_zip_code , only: [:destroy]
   def create
     @zip_code = ZipCode.new(zip_code_params)
     @zip_code.save
     @company_zone = @zip_code.company_zone
   end
+  def destroy
+    @company_zone = @zip_code.company_zone
+    @zip_code.destroy
+  end
   private
   def zip_code_params
     params.require(:zip_code).permit!
+  end
+  def set_zip_code
+    @zip_code = ZipCode.find(params[:id])
   end
 end
