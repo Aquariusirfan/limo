755c5eaa033dd7ba828abcffe030bf120be93ed3
Hamid <hamid@hamid>
2015-01-06 12:16:46 +0500

    zone rates controller

--- app/controllers/zone_rates_controller.rb
+++ app/controllers/zone_rates_controller.rb
@@ -10,18 +10,18 @@ class ZoneRatesController < ApplicationController
   def create
     #k == vehicle_type-id and v == zone_rate_value
     params[:hash].each do |k,v|
-      zone_rate = ZoneRate.where(:vehicle_type=>k,:destination_id=>params[:destination_id],:source_id=>params[:source_id])
+      zone_rate = ZoneRate.where(:vehicle_type=>k,:destination_id=>params[:destination_id],:source_id=>params[:source_id],:company_id=>current_user.company.id)
       if zone_rate.size > 0
         zone_rate.first.update_attribute(:zone_rate,v)
       else
-        ZoneRate.create(:vehicle_type_id=>k,:zone_rate=>v,:destination_id=>params[:destination_id],:source_id=>params[:source_id])
+        ZoneRate.create(:vehicle_type_id=>k,:zone_rate=>v,:destination_id=>params[:destination_id],:source_id=>params[:source_id],:company_id=>current_user.company.id)
       end
       
     end
   end
   
   def find_zone_rate
-    zone_rates = ZoneRate.where(:destination_id=>params[:destination_id],:source_id=>params[:source_id])
+    zone_rates = ZoneRate.where(:destination_id=>params[:destination_id],:source_id=>params[:source_id],:company_id=>current_user.company.id)
     if zone_rates.size > 0
       render :json => {'status'=>'success','zone_rates'=>zone_rates}
     else
