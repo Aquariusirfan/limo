66dee3f795720ae4221e3dad4c425a347c040004
Mahhek <dell@dell-Inspiron-N5010.(none)>
2015-01-06 18:23:50 +0500

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://http://limoservice.herokuapp.com'
+  host = 'http://limoservice.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.gmail.com",
